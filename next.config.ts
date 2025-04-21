56fb4d891c800fc51065e9c70c615b99c4d51cba
Leon Heuer <leon.heuer@otto.de>
2025-04-22 00:55:36 +0200

    modify gh pages workflow

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,12 @@
-import createMDX from '@next/mdx';
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
+import createMDX from '@next/mdx'
 
-const withMDX = createMDX({
-  extension: /\.mdx?$/,
-});
-
-/** @type {NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  pageExtensions: ['ts', 'tsx', 'mdx'],
-  experimental: {
-    mdxRs: true,
-  },
+  // Configure `pageExtensions` to include markdown and MDX files
+  pageExtensions: ['mdx', 'ts', 'tsx'],
 };
-
+const withMDX = createMDX({})
+ 
+// Merge MDX config with Next.js config
 export default withMDX(nextConfig);
