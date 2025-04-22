c56ac1543f0dc93300fae1e008aa254b16bf530a
Leon Heuer <leon.heuer@otto.de>
2025-04-22 02:01:00 +0200

    improve directory structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import TopIntroduction from "@/components/TopIntroduction";
-import AboutMeContainer from "@/components/AboutMeContainer";
+import TopIntroduction from "@/components/index-page/TopIntroduction";
+import AboutMeContainer from "@/components/index-page/AboutMeContainer";
 import AboutMe from "@/markdown/AboutMe.mdx";
-import WhatIDo from "@/components/WhatIDo";
-import FeaturedProjects from "@/components/FeaturedProjects";
-import Projects from "@/components/Projects";
-import ContactForm from "@/components/ContactForm";
+import WhatIDo from "@/components/index-page/WhatIDo";
+import FeaturedProjects from "@/components/index-page/FeaturedProjects";
+import Projects from "@/components/index-page/Projects";
+import ContactForm from "@/components/index-page/ContactForm";
 
 export default function Index() {
   return (
